4cbb1f0fb921bcdd1c576ab39d89537b21e8cbcf
MrJeroen <jeroen.gomes@student.uva.nl>
2016-11-16 19:22:04 +0100

    Add files via upload

--- score.py
+++ score.py
@@ -11,42 +11,45 @@ from collections import Counter
 traverser = 0
 room = 0
 score = 1000
-#
-# for i in range(7):
-#     j = 0
-#     for j in range(5):
-#         k = 0
-#         for k in range(4):
-#             if int(array[j][k][i][0].seats) < int(array[j][k][i][1].amount):
-#                 score += (int(array[j][k][i][0].seats) - int(array[j][k][i][1].amount))
-#
-# for vakken in students:
-#     overlap = []
-#     for room in range(7):
-#         days = 0
-#         for days in range(5):
-#             time = 0
-#             for time in range(4):
-#                 # Get first element of object.lessong
-#                 if vakken.vak1 == array[days][time][room][1].name:
-#                     overlap.append((time, days))
-#                 if vakken.vak2 == array[days][time][room][1].name:
-#                     overlap.append((time, days))
-#                 if vakken.vak3 == array[days][time][room][1].name:
-#                     overlap.append((time, days))
-#                 if vakken.vak4 == array[days][time][room][1].name:
-#                     overlap.append((time, days))
-#                 if vakken.vak5 == array[days][time][room][1].name:
-#                     overlap.append((time, days))
-#     something = [[x,overlap.count(x)] for x in set(overlap)]
-#     for elements in something:
-#         if elements[1] > 1:
-#             score -= elements[1]
+
+for i in range(7):
+    j = 0
+    for j in range(5):
+        k = 0
+        for k in range(4):
+            if int(array[j][k][i][0].seats) < int(array[j][k][i][1].amount):
+                score += (int(array[j][k][i][0].seats) - int(array[j][k][i][1].amount))
+
+for vakken in students:
+    overlap = []
+    for room in range(7):
+        days = 0
+        for days in range(5):
+            time = 0
+            for time in range(4):
+                # Get first element of object.lessong
+                if vakken.vak1 == array[days][time][room][1].name:
+                    overlap.append((time, days))
+                if vakken.vak2 == array[days][time][room][1].name:
+                    overlap.append((time, days))
+                if vakken.vak3 == array[days][time][room][1].name:
+                    overlap.append((time, days))
+                if vakken.vak4 == array[days][time][room][1].name:
+                    overlap.append((time, days))
+                if vakken.vak5 == array[days][time][room][1].name:
+                    overlap.append((time, days))
+    something = [[x,overlap.count(x)] for x in set(overlap)]
+    for elements in something:
+        if elements[1] > 1:
+            score -= elements[1]
 
 # Set counters
 day = 0
 room = 0
 time = 0
+hc =  'hc'
+wc = 'wc'
+pr = 'pr'
 
 for elements in (courses):
 # When working, remove last item (empty classes dont count)
@@ -58,7 +61,6 @@ for elements in (courses):
     for day in range(5):
         room = 0
         hc_count = 0
-        wc_count = 0
         for room in range(7):
             time = 0
             for time in range(4):
@@ -74,14 +76,15 @@ for elements in (courses):
                         # After second encounter, append
                         if hc_count > 1:
                             verdeling.append(day)
-        # Combination between double hc + pr, hc + wc, wc + pr.. also counts...                      
-                    # if type_vak != array[day][time][room][1].group_name:
-                    #     wc_count =+ 1
-                    #     if wc_count > 1:
-                    #         verdeling.append(day)
-
-                    # print array[day][time][room][1].name
-                    # print array[day][time][room][1].group_name
+                    if type_vak == hc:
+                        if array[day][time][room][1].group_name == pr:
+                                verdeling.append(day)
+                    if type_vak == hc:
+                        if array[day][time][room][1].group_name == wc:
+                                verdeling.append(day)
+                    if type_vak == wc:
+                        if array[day][time][room][1].group_name == pr:
+                                verdeling.append(day)
 
     print verdeling
     # Count the total extra occurences of a given name
