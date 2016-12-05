9e6fad59a662037b73b169e04371c2b167880e15
MrJeroen <jeroen.gomes@student.uva.nl>
2016-12-05 10:41:34 +0100

    Add files via upload

--- plot.py
+++ plot.py
@@ -1,21 +1,16 @@
 import numpy as np
-import pylab as plt
-# from score import *
+import matplotlib.pyplot as plt
 
 def Plot(list2):
-    # Values for x and y axis
-    plt.axis([0, 110, 50, 600])
-
+    plt.axis([0, 30, 50, 1000])
     plt.ylabel('Score')
     plt.xlabel('Iterations')
+
     j = 0
-
     for i in list2:
-        score = i
-        iteration = j
+        plt.scatter(j, i)
+        plt.plot(j, i)
         j += 1
-        # Produce scatter plot
-        plt.scatter(iteration, score)
 
-    # Show plot
+    plt.grid()
     plt.show()
