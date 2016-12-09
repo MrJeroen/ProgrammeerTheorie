e6407073366e8d598d29f3f9652966cd1a351c15
MrJeroen <jeroen.gomes@student.uva.nl>
2016-12-09 15:12:28 +0100

    Add files via upload

--- algoritme3.py
+++ algoritme3.py
@@ -7,25 +7,32 @@ list_return = []
 # List containing the highest scores that are encountered during iteration.
 list_highest = [0]
 
-# Open the best schedule provided by algorithm 1.
-with open ('algo1.py', 'rb') as fp:
-    best_list = pickle.load(fp)
-
-# Score the best schedule
-best_score = score(best_list)
-print best_score
+# # Open the best schedule provided by algorithm 1.
+# with open ('algo1.py', 'rb') as fp:
+#     best_list = pickle.load(fp)
+#
+# # Score the best schedule
+# best_score = score(best_list)
+# print best_score
 
 # Iterate through the hillclimber algorithm 'x' amount of times.
-for i in range(10):
+for i in range(5):
+    with open ('algo5.py', 'rb') as fp:
+        best_list = pickle.load(fp)
     # For every iteration, use the hillclimber method 100 times.
-    for j in range(1000):
+    for j in range(100):
+        with open ('algo5.py', 'rb') as fp:
+            best_list = pickle.load(fp)
+        # Score the best schedule
+        best_score = score(best_list)
+        # print best_score
         # Get a (new) shuffeled list based on the shuffle method.
         new_list = hillclimber(best_list, 0, 19, 20, 59, 60, 139)
         # Score this new list.
         new_score = score(new_list)
         # Add new score to a list.
         list_return.append(new_score)
-        print new_score
+        # print new_score
 
         # If the new score is better compared to the previous highest.
         if new_score > best_score:
@@ -38,9 +45,8 @@ for i in range(10):
             best_schedule = new_list
             best_score = score(best_schedule)
             print best_score
-
             # Create a file with the best list for future algorithms.
-            with open('algo3.py', 'wb') as fp:
+            with open('algo5.py', 'wb') as fp:
                 pickle.dump(best_schedule, fp)
 
 print list_highest
