60ca387e643be5004ec496a5572939c37d0e5f35
MrJeroen <jeroen.gomes@student.uva.nl>
2016-12-15 13:13:04 +0100

    Add files via upload

--- algoritme3.py
+++ algoritme3.py
@@ -6,41 +6,46 @@ import pickle
 list_return = []
 # List containing the highest scores that are encountered during iteration.
 list_highest = [0]
+# Lists for plotting.
 xaxis = []
 extra =[]
 
+# Create a random schedule and save.
 random = ShuffledList(lessons)
-# Create a file with a random schedule to start the hillclimber.
 with open('algo5.py', 'wb') as fp:
     pickle.dump(random, fp)
 
-# Iterate through the hillclimber algorithm 'x' amount of times.
-for i in range(1, 5):
+# Iterate through the hillclimber algorithm
+for i in range(1, 11):
     xaxis.append(i)
-    print "blablabla"
+    # Open random schedule and make this the base schedule. After the first
+    # run this will be the bests schedule generated from the loop below.
     with open ('algo5.py', 'rb') as fp:
         best_list = pickle.load(fp)
 
-    for j in range(10):
-        # Score the best schedule.
-        best_score = score(best_list)
-        print best_score
-        # Get a (new) shuffeled list based on the shuffle method.
+    # Score the base schedule.
+    best_score = score(best_list)
+    print best_score
+
+    # Make 'x' changes on the base schedule
+    for j in range(100):
+        # Change two objects in the schedule & score it.
         new_list = hillclimber(best_list, 0, 19, 20, 59, 60, 139)
-        # Score this new list.
         new_score = score(new_list)
-        print "hoi1"
-        print new_score
-        # If the new score is better compared to the previous highest.
+        # print new_score
+
+        # If a schedule is improved, save it
         if new_score > best_score:
-            print "hoi2"
-            best_list = new_list
-            # Create a file with the best list for future algorithms.
-            with open('algo5.py', 'wb') as fp:
-                pickle.dump(new_list, fp)
-
-
+            # list_highest.insert(0, new_score)
+            if new_score > list_highest[0]:
+                list_highest.insert(0, new_score)
+                best_list = new_list
+                # Create a file with the best list for future algorithms.
+                with open('algo5.py', 'wb') as fp:
+                    pickle.dump(best_list, fp)
+    # Create list for plot
     extra.append(best_score)
 
-print extra
-#Plot2(xaxis, extra, 'Hillclimber')
+print list_highest
+# print extra
+Plot2(xaxis, extra, 'Hillclimber')
